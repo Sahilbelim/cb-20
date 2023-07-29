32aedc279befbf3d2f1f7afade3acff4ce8b7ae9
parampatel07 <iamparam102@gmail.com>
2023-07-29 09:55:53 +0530

    while

---
+++ while5.c
@@ -0,0 +1,28 @@
+// Write a programe to print following pattern
+// 0, 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ... 1000
+#include <stdio.h>
+void main()
+{
+     int number = 0;
+     int count = 1;
+
+     while(number <= 1000)
+     {
+          printf("%d ", number); // 0
+          number = number + count;
+          count = count + 1;
+     } // 2
+     // printf("%d ",number);// 1
+     // number = number + count ;
+     // count = count + 1;//3
+     // printf("%d ",number);// 3
+     // number = number + count;
+     // count = count + 1;
+     // printf("%d ",number);// 6
+     // number = number + count;
+     // printf("%d ",number);// 10
+     // number = number + 5 ;
+     // printf("%d ",number);// 15
+     // number = number + 6 ;
+     // printf("%d ",number);// 21
+}
